d1a2dc865a9e16e0b6a2812ed3fc37e08b793cd9
Annasuzanne <sanneromijn1@gmail.com>
2024-03-27 14:32:13 +0100

    19

--- carousel.js
+++ carousel.js
@@ -33,3 +33,60 @@
 //     }
 //   });
   
+
+
+
+// var swiper = new Swiper(".mySwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 30,
+//     slidesPerGroup: 3,
+//     loop: true,
+//     loopFillGroupWithBlank: true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//   });
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 25,
+        // slidesPerGroup: 3,
+        loop: true,
+        centerSlide: true, 
+        fade: true,
+        grabCursor: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            dynamicBullets: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+
+
+        breakpoints:{
+            0:{
+                slidesPerView: 1,
+            },
+            520:{
+                slidesPerView: 2,
+            },
+           950:{
+                slidesPerView: 3,
+            },
+        },
+
+
+    });
+});
