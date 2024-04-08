506aa1c630d336f6d4e2f2e209cdbc62fd1a1680
Annasuzanne <sanneromijn1@gmail.com>
2024-04-08 15:20:44 +0200

    32

--- carousel.js
+++ carousel.js
@@ -1,56 +1,3 @@
-// document.addEventListener('DOMContentLoaded', function () {
-//     let multipleCardCarousel = document.querySelector("#carouselExampleControls");
-
-
-//     if (window.matchMedia("(min-width: 768px)").matches) {
-//       let carousel = new bootstrap.Carousel(multipleCardCarousel, {
-//         interval: false, // Disable automatic sliding
-//         wrap: false, // Prevent wrapping at the end
-//       });
-
-
-//       let carouselWidth = document.querySelector(".carousel-inner").scrollWidth;
-//       let cardWidth = document.querySelector(".carousel-item").offsetWidth;
-//       let scrollPosition = 0;
-
-
-//       document.querySelector("#carouselExampleControls .carousel-control-next").addEventListener("click", function () {
-//         if (scrollPosition < carouselWidth - cardWidth * 4) {
-//           scrollPosition += cardWidth;
-//           document.querySelector("#carouselExampleControls .carousel-inner").scroll({ left: scrollPosition, behavior: 'smooth' });
-//         }
-//       });
-
-
-//       document.querySelector("#carouselExampleControls .carousel-control-prev").addEventListener("click", function () {
-//         if (scrollPosition > 0) {
-//           scrollPosition -= cardWidth;
-//           document.querySelector("#carouselExampleControls .carousel-inner").scroll({ left: scrollPosition, behavior: 'smooth' });
-//         }
-//       });
-//     } else {
-//       multipleCardCarousel.classList.add("slide");
-//     }
-//   });
-  
-
-
-
-// var swiper = new Swiper(".mySwiper", {
-//     slidesPerView: 3,
-//     spaceBetween: 30,
-//     slidesPerGroup: 3,
-//     loop: true,
-//     loopFillGroupWithBlank: true,
-//     pagination: {
-//       el: ".swiper-pagination",
-//       clickable: true,
-//     },
-//     navigation: {
-//       nextEl: ".swiper-button-next",
-//       prevEl: ".swiper-button-prev",
-//     },
-//   });
 
 
 
@@ -90,3 +37,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     });
 });
+
+
+
+
+var swiper = new Swiper('.swiper-container', {
+    // Opties voor de carrousel
+    loop: true, // Oneindige loop
+    slidesPerView: 'auto', // Pas aantal zichtbare slides aan op basis van schermgrootte
+    spaceBetween: 20, // Afstand tussen slides
+    autoplay: {
+      delay: 5000, // Interval tussen slides in milliseconden (hier 5 seconden)
+    },
+    pagination: {
+      el: '.swiper-pagination', // Element voor de paginatie
+      clickable: true, // Klikbaarheid van paginatie
+    },
+  });
