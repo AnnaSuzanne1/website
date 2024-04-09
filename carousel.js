8d1571f5bbdabc7afd28ed77404e21c01abf7b44
Annasuzanne <sanneromijn1@gmail.com>
2024-04-09 11:53:46 +0200

    34

--- carousel.js
+++ carousel.js
@@ -39,5 +39,27 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+function toggleTheme() {
+    console.log("Toggle Theme function is called");
+    var body = document.body;
+    // Controleer of het 'data-bs-theme' attribuut al is ingesteld op 'dark'
+    if (body.getAttribute('data-bs-theme') === 'dark') {
+        // Verwijder het 'data-bs-theme' attribuut als het al 'dark' is
+        body.removeAttribute('data-bs-theme');
+    } else {
+        // Voeg het 'data-bs-theme' attribuut toe met de waarde 'dark'
+        body.setAttribute('data-bs-theme', 'dark');
+    }
+}
 
+// Selecteer alle elementen met de klasse "toggleThemeBtn"
+var toggleThemeBtns = document.getElementsByClassName('toggleThemeBtn');
+console.log("Number of toggleThemeBtns:", toggleThemeBtns.length);
 
+// Itereer door alle gevonden knoppen en voeg een klikgebeurtenis toe aan elk van hen
+for (var i = 0; i < toggleThemeBtns.length; i++) {
+    toggleThemeBtns[i].addEventListener('click', function() {
+        console.log("Button is clicked");
+        toggleTheme();
+    });
+}
