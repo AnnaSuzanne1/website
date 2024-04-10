3f710b09e6d911605a4147d4f6990038ed20a55b
Annasuzanne <sanneromijn1@gmail.com>
2024-04-10 10:28:59 +0200

    37

--- carousel.js
+++ carousel.js
@@ -1,12 +1,11 @@
 
 
 
-
+// Swiper 
 document.addEventListener('DOMContentLoaded', function() {
     var swiper = new Swiper(".mySwiper", {
         slidesPerView: 3,
         spaceBetween: 25,
-        // slidesPerGroup: 3,
         loop: true,
         centerSlide: true, 
         fade: true,
@@ -39,24 +38,20 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 
+
+// Dark theme 
 function toggleTheme() {
     console.log("Toggle Theme function is called");
     var body = document.body;
-    // Controleer of het 'data-bs-theme' attribuut al is ingesteld op 'dark'
     if (body.getAttribute('data-bs-theme') === 'dark') {
-        // Verwijder het 'data-bs-theme' attribuut als het al 'dark' is
         body.removeAttribute('data-bs-theme');
     } else {
-        // Voeg het 'data-bs-theme' attribuut toe met de waarde 'dark'
         body.setAttribute('data-bs-theme', 'dark');
     }
 }
 
-// Selecteer alle elementen met de klasse "toggleThemeBtn"
 var toggleThemeBtns = document.getElementsByClassName('toggleThemeBtn');
-console.log("Number of toggleThemeBtns:", toggleThemeBtns.length);
 
-// Itereer door alle gevonden knoppen en voeg een klikgebeurtenis toe aan elk van hen
 for (var i = 0; i < toggleThemeBtns.length; i++) {
     toggleThemeBtns[i].addEventListener('click', function() {
         console.log("Button is clicked");
