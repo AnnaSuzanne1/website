6bc7c0f4ab168477f31e89ba26c635f85f667aca
Annasuzanne <sanneromijn1@gmail.com>
2024-10-21 17:08:00 +0200

    130

---
+++ support.js
@@ -0,0 +1,18 @@
+  // Wacht totdat de pagina geladen is
+  window.addEventListener('load', function() {
+    // Controleer of de URL een hash heeft (#verwijzing-servicedesk)
+    const elementId = window.location.hash.substring(1);
+    if (elementId) {
+      // Zoek het element met de bijbehorende id
+      const targetElement = document.getElementById(elementId);
+      if (targetElement) {
+        // Voeg de highlight-klasse toe
+        targetElement.classList.add('highlight');
+
+        // Verwijder de highlight-klasse na 2 seconden
+        setTimeout(() => {
+          targetElement.classList.remove('highlight');
+        }, 2000); // Duur van het highlight-effect
+      }
+    }
+  });
